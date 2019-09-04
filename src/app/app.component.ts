fe77f1f4e6a1db673e669d614d3a205c23974589
lcarlso2 <lcarlso2@my.westga.edu>
2019-09-04 14:18:50 -0400

    Started making a custom component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'lm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ListingManagement';
+export class AppComponent{
+  pageTitle = "Listing Management";
 }
