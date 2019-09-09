e5871c75cc04108056dbac65b4d9ddced34594f8
lcarlso2 <lcarlso2@my.westga.edu>
2019-09-09 15:15:42 -0400

    show/hide id

--- src/app/houses/house-list.component.ts
+++ src/app/houses/house-list.component.ts
@@ -10,6 +10,10 @@ import { House } from './house.model';
 })
 export class HouseListComponent{
   isDisabled:boolean = true;
+
+  showID:boolean = true;
+
+  
   houses:House[] = [
       new House(1,"123 Maple st., Carrollton, GA 30118", "Beautiful House", 234000, new Date(2019,8,9),4,3.5,"7704567890","./assets/images/house1.jpeg"),
       new House(2,"234 Maple st., Carrollton, GA 30118", "Ugly House", 123000, new Date(2019,8,9),4,3.5,"4567891232","./assets/images/house2.jpeg"),
@@ -19,4 +23,13 @@ export class HouseListComponent{
       new House(6,"567 Maple st., Carrollton, GA 30118", "Best House", 987867, new Date(2019,8,9),4,3.5,"7705687665","./assets/images/house6.jpeg"),
       new House(7,"777 Maple st., Carrollton, GA 30118", "Last House", 456789, new Date(2019,8,9),4,3.5,"7700987654","./assets/images/house7.jpeg")
   ]
+
+
+
+
+  onShowIDClicked():void{
+    this.showID = !this.showID;
+  }
 }
+
+
