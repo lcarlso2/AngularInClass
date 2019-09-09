3535545cacb3e3bcd56177bb181348d5672d8de8
lcarlso2 <lcarlso2@my.westga.edu>
2019-09-09 15:05:49 -0400

    buttons

--- src/app/houses/house-list.component.ts
+++ src/app/houses/house-list.component.ts
@@ -9,6 +9,7 @@ import { House } from './house.model';
   styleUrls: ['./house-list.component.css'],
 })
 export class HouseListComponent{
+  isDisabled:boolean = true;
   houses:House[] = [
       new House(1,"123 Maple st., Carrollton, GA 30118", "Beautiful House", 234000, new Date(2019,8,9),4,3.5,"7704567890","./assets/images/house1.jpeg"),
       new House(2,"234 Maple st., Carrollton, GA 30118", "Ugly House", 123000, new Date(2019,8,9),4,3.5,"4567891232","./assets/images/house2.jpeg"),
